d1329690eb7c09a91e6cd18e6b3338d49da981e3
Nwakaeme Cynthia Ebere <nwakaemecynthia@gmail.com>
2022-07-08 21:36:09 +0100

    'Implemented multiple fileUpload'

--- file-upload/server/upload.router.js
+++ file-upload/server/upload.router.js
@@ -2,9 +2,18 @@ const express = require('express');
 const router = express.Router();
 const upload = require('./upload.setting')
 
+/*SINGLE FILE UPLOAD*/
 router.post('/picture', upload.single('picture'),(req, res) => {
   const avatar = req.file;
   const userObj = {...req.body, avatar}
+  res.status(200).json({data: userObj,status: 'SUCCESS', message: 'Profile Successfully upload'});
+});
+
+/*MULTIPLE FILE UPLOAD*/
+router.post('/pictures', upload.array('pictures', 3),(req, res) => {
+  const avatar = req.files;
+  console.log(avatar)
+  const userObj = {...req.body, ...avatar}
   res.status(200).json({data: userObj,status: 'SUCCESS', message: 'Profile Successfully upload'});
 });
 
